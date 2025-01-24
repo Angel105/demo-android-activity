b343daa6c0d599f01d4e38447a5f578153f972af
Borzoi <siteandstock@gmail.com>
2025-01-24 20:32:53 +0100

    [3] create and use Toast in Activity

--- app/src/main/java/com/example/activitytest/FirstActivity.kt
+++ app/src/main/java/com/example/activitytest/FirstActivity.kt
@@ -1,6 +1,8 @@
 package com.example.activitytest
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +12,9 @@ class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.first_layout)
+        val button1: Button = findViewById(R.id.button1)
+        button1.setOnClickListener {
+            Toast.makeText(this, "You clicked Button 1", Toast.LENGTH_SHORT).show()
+        }
     }
 }
