e46a917247a5b11343a754044892be7f91b5c2c3
Borzoi <siteandstock@gmail.com>
2025-01-29 23:08:13 +0100

    [14]   register the contract using the modern registerForActivityResult API rather than the obsolete startActivityForResult
    Return Data to the Last Activity

--- app/src/main/java/com/example/activitytest/FirstActivity.kt
+++ app/src/main/java/com/example/activitytest/FirstActivity.kt
@@ -2,26 +2,39 @@ package com.example.activitytest
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.activitytest.databinding.FirstLayoutBinding
 
 class FirstActivity : AppCompatActivity() {
 
     private lateinit var binding: FirstLayoutBinding
+    private lateinit var startForResult: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = FirstLayoutBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
+
+        // Register the contract using the modern registerForActivityResult API
+        startForResult = registerForActivityResult(ActivityResultContracts
+            .StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val data = result.data
+                val returnedData = data?.getStringExtra("data_return")
+                Toast.makeText(this, returnedData, Toast.LENGTH_SHORT).show()
+                Log.d("FirstActivity", "returned data is $returnedData")
+            }
+
+        }
         binding.button1.setOnClickListener {
-            val data = "Hello SecondActivity"
             val intent = Intent(this, SecondActivity::class.java)
-            intent.putExtra("extra_data", data)
-            startActivity(intent)
+            startForResult.launch(intent)
         }
     }
 
--- app/src/main/java/com/example/activitytest/SecondActivity.kt
+++ app/src/main/java/com/example/activitytest/SecondActivity.kt
@@ -1,24 +1,30 @@
 package com.example.activitytest
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.activitytest.databinding.SecondActivityBinding
 
 class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: SecondActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.second_activity)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main2)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = SecondActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button2.setOnClickListener {
+            Log.d("SecondActivity", "button clicked")
+            val intent = Intent()
+            intent.putExtra("data_return", "Hello FirstActivity")
+            setResult(RESULT_OK, intent)
+            finish()
         }
-        val extraData = intent.getStringExtra("extra_data")
-        Log.d("SecondActivity", "extra data is $extraData")
+
     }
 
 }
