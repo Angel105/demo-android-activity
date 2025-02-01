09827c1854874b1729c490a94160be89553eb9d1
Borzoi <siteandstock@gmail.com>
2025-02-01 18:04:55 +0100

    [24]  exiting the App from Anywhere: create a singleton ActivityCollector to host the collection of activities (use singleton because we only need one collection in one app), create methods addActivity() to add activity in the ArrayList, removeActivity() to remove activity, finishAll() to destroy all the activities in ArrayList (note that before destroying an activity, we need to use activity.isFinishing to check if the activity is already in the process of getting destroyed, if not we will call finish() method to destroy it)
    The onCreate() method of BaseActivity will call the addActivity() method in ActivityController to add the current activity to the collection. Then onDestroy() in BaseActivity gets overridden with calling the removeActivity() ActivityController to remove a soon to be destroyed activity from the collection.
    
    Exit the app anywhere by calling  ActivityController.finishAll() method.
    
    Killing the current process with : android.os.Process.killProcess(android.os.Process.myPid()) has a number of potential issues:
    
    - Abrupt Termination: killProcess() is a very abrupt way to end your app. It doesn't give your app a chance to perform any cleanup tasks (e.g., saving data, releasing resources, etc.).
    - Unpredictable Behavior: Forcefully killing the process can lead to unpredictable behavior in some cases, especially if your app interacts with other components or services.
    - Not Standard Practice: This is not the standard way Android apps are designed to exit.
    - Data loss: If the app has unsaved data, it will be lost.
    - Resource leaks: If the app has acquired resources, they may not be released properly.
    - Unexpected behavior: The app may not be able to perform cleanup tasks, which can lead to unexpected behavior.
    - Not recommended: This method is not recommended by Android developers.

--- app/src/main/java/com/example/activitytest/ThirdActivity.kt
+++ app/src/main/java/com/example/activitytest/ThirdActivity.kt
@@ -5,17 +5,28 @@ import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.activitytest.databinding.ThirdLayoutBinding
 
 class ThirdActivity : BaseActivity() {
+
+    private lateinit var binding: ThirdLayoutBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d("ThirdActivity", "Task id is $taskId")
         enableEdgeToEdge()
-        setContentView(R.layout.third_layout)
+        binding = ThirdLayoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main3)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.button3.setOnClickListener {
+            ActivityCollector.finishAll()
+        }
+
     }
 }
