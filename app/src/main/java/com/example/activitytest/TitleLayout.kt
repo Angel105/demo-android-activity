c69d109be41bce39e4ee36a4d7bdcd3facd989a0
flyin <flying.falcon@outlook.com>
2025-02-04 19:32:51 +0100

    [47] register click event listener for the buttons in the toolbar, inflate the layout and get the binding object, registers the click event listener for back and edit button, clicking the back button will destroy the current activity,  clicking the edit button   will show a toast.
    Notice that the context param that TitleLayout takes is actually an instance of Activity.  Cast context to Activity using the keyword as, call finish() to destroy the current activity.

--- app/src/main/java/com/example/activitytest/TitleLayout.kt
+++ app/src/main/java/com/example/activitytest/TitleLayout.kt
@@ -1,13 +1,25 @@
 package com.example.activitytest
 
+import android.app.Activity
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
+import android.widget.Toast
+import com.example.activitytest.databinding.TitleLayoutBinding
 
 class TitleLayout(context: Context, attrs: AttributeSet): LinearLayout(context, attrs) {
 
+    private val binding: TitleLayoutBinding
+
     init {
-        LayoutInflater.from(context).inflate(R.layout.title_layout, this)
+        binding = TitleLayoutBinding.inflate(LayoutInflater.from(context), this, true)
+        binding.titleBack.setOnClickListener {
+            val activity = context as Activity
+            activity.finish()
+        }
+        binding.titleEdit.setOnClickListener {
+            Toast.makeText(context, "You clicked Edit button", Toast.LENGTH_SHORT).show()
+        }
     }
 }
