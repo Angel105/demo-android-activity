623202d00fdad024f33c3e2f7d7620341be8d134
flyin <flying.falcon@outlook.com>
2025-02-06 17:53:57 +0100

    [51] optimize the efficiency of the ListView - step 2:  every time getView() gets called, findViewById() method will get called to get reference of the view.  Use the inner class ViewHolder to optimize this, to cache the ImageView and TextView instance.
    The setTag() method of View will set the tag to be the instance of ViewHolder. When convertView is not null, getTag() will get the instance of ViewHolder. This instance has the instances of TextView and ImageView. Thus there is no need to use findViewById() again.
    
    After these two steps of optimization, this customized ListView will perform much more efficiently.

--- app/src/main/java/com/example/activitytest/FruitAdapter.kt
+++ app/src/main/java/com/example/activitytest/FruitAdapter.kt
@@ -10,19 +10,26 @@ import android.widget.TextView
 
 class FruitAdapter(activity: Activity, val resourceId: Int, data: List<Fruit>): ArrayAdapter<Fruit>(activity, resourceId, data) {
 
+    inner class ViewHolder(val fruitImage: ImageView, val fruitName: TextView)
+
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val view: View
+        val viewHolder: ViewHolder
         if (convertView==null) {
             view = LayoutInflater.from(context).inflate(resourceId, parent, false)
+
+            val fruitImage: ImageView = view.findViewById(R.id.fruitImage)
+            val fruitName: TextView = view.findViewById(R.id.fruitName)
+            viewHolder = ViewHolder(fruitImage, fruitName)
+            view.tag = viewHolder
         } else {
             view = convertView
+            viewHolder = view.tag as ViewHolder
         }
-        val fruitImage: ImageView = view.findViewById(R.id.fruitImage)
-        val fruitName: TextView = view.findViewById(R.id.fruitName)
         val fruit = getItem(position) // Get the fruit instance of the current item
         if (fruit != null) {
-            fruitImage.setImageResource(fruit.imageId)
-            fruitName.text = fruit.name
+            viewHolder.fruitImage.setImageResource(fruit.imageId)
+            viewHolder.fruitName.text = fruit.name
         }
 
         return view
