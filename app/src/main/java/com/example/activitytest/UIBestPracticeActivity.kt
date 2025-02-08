3a9542f279a5a95127781ef4ab458fc275e615b6
flyin <flying.falcon@outlook.com>
2025-02-08 18:34:34 +0100

    [60] build a beautiful Chat User Interface
    There are only two fields in Msg class to store message content and message type. Message type has two values: TYPE_RECEIVED and TYPE_SENT. (const can only be used in singleton, companion object, or top-level method.)
    
    Initialize data in initMsg().  The send button event listener gets the content of EditText view and if the content is not empty string, a new instance of Msg will be created and added to msgList.  The notifyItemInserted() method will notify the change and refresh the RecyclerView to show the latest message.
    
    (You can also use the notifyDataSetChanged() method of
     the adapter which will refresh all the visible elements in the RecyclerView. Thus  whether it is insertion, deletion, or updating, UI will show the latest data, but this is relatively slow)
    
    The scrollToPosition() method will scroll
    the RecyclerView to the last item. This is to make sure the latest message will appear.
    
    The setText() method clears the EditText view.

--- app/src/main/java/com/example/activitytest/UIBestPracticeActivity.kt
+++ app/src/main/java/com/example/activitytest/UIBestPracticeActivity.kt
@@ -1,20 +1,58 @@
 package com.example.activitytest
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.activitytest.databinding.ActivityUiBestPracticeBinding
 
-class UIBestPracticeActivity : AppCompatActivity() {
+class UIBestPracticeActivity : AppCompatActivity(), View.OnClickListener {
+
+    private val msgList = ArrayList<Msg>()
+
+    private var adapter: MsgAdapter? = null
+
+    private lateinit var binding: ActivityUiBestPracticeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_ui_best_practice)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityUiBestPracticeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initMsg()
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.layoutManager = layoutManager
+        adapter = MsgAdapter(msgList)
+        binding.recyclerView.adapter = adapter
+        binding.send.setOnClickListener(this)
+
+    }
+
+    private fun initMsg() {
+        val msg1 = Msg("Hello, folks!", Msg.TYPE_RECEIVED)
+        val msg2 = Msg("Hello! Who are you?", Msg.TYPE_SENT)
+        val msg3 = Msg("It's me, Rob!, Nice talking to you again, folks!", Msg.TYPE_RECEIVED)
+        msgList.addAll(listOf(msg1, msg2, msg3))
+    }
+
+    override fun onClick(v: View?) {
+        when (v) {
+            binding.send -> {
+                val content = binding.inputText.text.toString()
+                Log.d("UIBestPracticeActivity", "onClick, text $content")
+                if (content.isNotEmpty()) {
+                    val msg = Msg(content, Msg.TYPE_SENT)
+                    msgList.add(msg)
+                    adapter?.notifyItemInserted(msgList.size - 1) // when there is a new msg, refresh RecyclerView
+                    binding.recyclerView.scrollToPosition(msgList.size - 1) // scroll RecyclerView to the last item
+                    binding.inputText.text.clear() // clear the EditText view
+                }
+            }
         }
     }
 }
