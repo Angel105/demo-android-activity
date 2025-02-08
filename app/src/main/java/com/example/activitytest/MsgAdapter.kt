3eb48fd6eb16d53ce7dd0317a298c733f057ad4e
flyin <flying.falcon@outlook.com>
2025-02-08 20:04:29 +0100

    [62]  optimize with Sealed Class: create sealed class  MsgViewHolder and two subclasses
    There is no need to add else statement in the when(holder) {...} condition check of the onBindViewHolder method.
    
    When the when statement gets a sealed class as argument, Kotlin compiler will check what are the sub classes that inherits this class, then check if all the cases are covered which can eliminate the else statement.
    
    If we add Unknown class that inherits MsgViewHolder without adding the corresponding condition check in onBindViewHolder(), compiler will fail to compile.
    
    The sealed class and its sub classes can only be defined at the top level of the same file and cannot be embedded in other classes which is a limitation of the sealed class’s implementation.

--- app/src/main/java/com/example/activitytest/MsgAdapter.kt
+++ app/src/main/java/com/example/activitytest/MsgAdapter.kt
@@ -1,20 +1,10 @@
 package com.example.activitytest
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class MsgAdapter(val msgList: List<Msg>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-
-    inner class LeftViewHolder(view: View): RecyclerView.ViewHolder(view) {
-        val leftMsg: TextView = view.findViewById(R.id.leftMsg)
-    }
-
-    inner class RightViewHolder(view: View): RecyclerView.ViewHolder(view) {
-        val rightMsg: TextView = view.findViewById(R.id.rightMsg)
-    }
 
     override fun getItemViewType(position: Int): Int {
         val msg = msgList[position]
@@ -35,7 +25,6 @@ class MsgAdapter(val msgList: List<Msg>): RecyclerView.Adapter<RecyclerView.View
         when(holder) {
             is LeftViewHolder -> holder.leftMsg.text = msg.content
             is RightViewHolder -> holder.rightMsg.text = msg.content
-            else -> throw IllegalArgumentException()
         }
     }
 
