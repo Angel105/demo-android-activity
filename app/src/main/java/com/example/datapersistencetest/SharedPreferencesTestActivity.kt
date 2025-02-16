088680d4e10b6fd0c625e172412b48359219a440
Borzoi <siteandstock@gmail.com>
2025-02-16 17:25:31 +0100

    [78] save/read Data to/from SharedPreferences, which uses key-value pairs to store data (much like localstorage).
     The method  getSharedPreferences() in Context Class takes two params. The first param is used to specify the file name of the SharedPreferences.  SharedPreferences files are under the path /data/data/<package name>/shared_prefs/. (click synchronize to have the shared_prefs folder appear in the Device Explorer).
    
     For reading data,  all of the get methods (getString, getInt, getBoolean, etc.) will take two params: the first param is the key, which is corresponding to the key that is used when saving the data; the second param is the default value, which will be returned if no data can be found with the key passed in.
    
     What is SharedPreferences used for?
    * Save User Preferences: Store user settings, like theme choices, language preferences, etc.
    * Persist App State: Save the state of your app so that it can resume where the user left off.
    * Cache Data: Store small amounts of data to avoid having to re-fetch it from a network or database.

---
+++ app/src/main/java/com/example/datapersistencetest/SharedPreferencesTestActivity.kt
@@ -0,0 +1,56 @@
+package com.example.datapersistencetest
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.activitytest.R
+import com.example.activitytest.databinding.ActivitySharedPreferencesTestBinding
+
+class SharedPreferencesTestActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySharedPreferencesTestBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivitySharedPreferencesTestBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.saveButton.setOnClickListener {
+            // Save data
+            Log.d("SharedPreferencesTest", "Saving data...")
+            val sharedPreferences = getSharedPreferences("data", Context.MODE_PRIVATE)
+            val editor = sharedPreferences.edit()
+            editor.putString("name", "Andrew")
+            editor.putInt("age", 28)
+            editor.putBoolean("married", false)
+            editor.apply()
+
+            restoreData(sharedPreferences)
+        }
+
+        binding.restoreButton.setOnClickListener {
+            Log.d("SharedPreferencesTest", "Restoring data...")
+            val sharedPreferences = getSharedPreferences("data", Context.MODE_PRIVATE)
+            restoreData(sharedPreferences)
+        }
+
+    }
+
+    private fun restoreData(sharedPreferences: SharedPreferences) {
+        // Retrieve data saved data
+        val myString = sharedPreferences.getString("name", "Default Value")
+        val myInt = sharedPreferences.getInt("age", 0)
+        val myBoolean = sharedPreferences.getBoolean("married", false)
+
+        // Display data
+        Toast.makeText(this, "Name: $myString, Age: $myInt, Married: $myBoolean", Toast.LENGTH_LONG)
+            .show()
+        Log.d("SharedPreferencesTest", "Name: $myString, Age: $myInt, Married: $myBoolean")
+    }
+
+}
