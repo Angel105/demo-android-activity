6118062e9f523609b69ab80787222ddb0f6b2a26
Borzoi <siteandstock@gmail.com>
2025-02-16 18:05:07 +0100

    [79] implement remembering password for our BroadcastBestPractice login UI project using SharedPreference to store account, password and remember_password values. This functionality is for demonstration purpose and cannot be used in real world project. Because it is not secure to save password in plain text in SharedPreferences.

--- app/src/main/java/com/example/broadcasttest/LoginActivity.kt
+++ app/src/main/java/com/example/broadcasttest/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.broadcasttest
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
@@ -16,12 +17,32 @@ class LoginActivity : BaseActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val prefs = getPreferences(Context.MODE_PRIVATE)
+        val isRemember =  prefs.getBoolean("remember_password", false)
+        if (isRemember) {
+            // Set the account and password info in the EditText views
+            val account = prefs.getString("account", "")
+            val password= prefs.getString("password", "")
+            binding.accountEdit.setText(account)
+            binding.passwordEdit.setText(password)
+            binding.rememberPass.isChecked = true
+        }
+
         binding.login.setOnClickListener {
             val account = binding.accountEdit.text.toString()
             val password= binding.passwordEdit.text.toString()
 
             // Correct account is student and password is student
             if (account=="student" && password=="student") {
+                val editor = prefs.edit()
+                if (binding.rememberPass.isChecked) {
+                    editor.putBoolean("remember_password", true)
+                    editor.putString("account", account)
+                    editor.putString("password", password)
+                } else {
+                    editor.clear()
+                }
+                editor.apply()
                 val intent = Intent(this, BroadcastTestMainActivity::class.java)
                 startActivity(intent)
                 finish()
