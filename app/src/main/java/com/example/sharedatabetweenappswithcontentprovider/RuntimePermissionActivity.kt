83db518c598564f0dc6cac475db6a64dc2081206
Borzoi <siteandstock@gmail.com>
2025-02-19 16:32:23 +0100

    [93] request Permissions at Runtime: treat dangerous permission as runtime permissions for Android 6.0 and above. Essentially, runtime permission is to ask user to grant permission for the app to do risky action while the app is running.
     The first step is check if the permission is granted or not by ContextCompact.checkSelfPermission().  This method takes two params, the first param is Context and the second param is the name of the permission, for example, Manifest.permission.CALL_PHONE.  Then, compare the returned value with PERMISSION_GRANTED to see if the user has granted this permission or not.
    
     If permission granted, then continue executing the calling logic (the call() function ). If permission not granted, then use ActivityCompact.requestPermissions() to request permission. This method takes three params: the first param should be an instance of Activity; the second param is a String array which will host the permission name we’re requesting; and the third param is the request code which only requires a unique value and here we pass in 1.
    
     requestPermission() will bring up a permission request dialog to let the user grant or decline our request.
    
     The result will be encapsulated in grantResults argument and passed to onRequestPermissionsResult() callback.
    
     Then we can check the final result of the request, if user grants the request then we can call call() to make phone calls, if not then we have to give up and show a message to the user.
    
     Run the app and click Make Call btn. Since this is the first time, thus permission is not granted yet, and the dialog for permission will show up.  If we click Deny, the result will be as shown in the Toast message "You denied the permission".
    
     Next time we click Make Call btn, the dialog for permission request will show up again, click Allow this time to reach to the dial screen.

--- app/src/main/java/com/example/sharedatabetweenappswithcontentprovider/RuntimePermissionActivity.kt
+++ app/src/main/java/com/example/sharedatabetweenappswithcontentprovider/RuntimePermissionActivity.kt
@@ -1,10 +1,15 @@
 package com.example.sharedatabetweenappswithcontentprovider
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.activitytest.databinding.ActivityRuntimePermissionBinding
 
 class RuntimePermissionActivity : AppCompatActivity() {
@@ -18,14 +23,39 @@ class RuntimePermissionActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.makeCall.setOnClickListener {
-            try {
-                val intent = Intent(Intent.ACTION_CALL)
-                intent.data = Uri.parse("tel:0123456789")
-                startActivity(intent)
-            } catch (e: SecurityException) {
-                e.printStackTrace()
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CALL_PHONE), 1)
+            } else {
+                call()
             }
         }
+    }
 
+    private fun call() {
+        try {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:0123456789")
+            startActivity(intent)
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray,
+        deviceId: Int
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults, deviceId)
+        when (requestCode) {
+            1 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    call()
+                } else {
+                    Toast.makeText(this, "You denied the permission", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
