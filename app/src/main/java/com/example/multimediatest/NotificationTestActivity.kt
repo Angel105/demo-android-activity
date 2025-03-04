fbdfd1d5d94be5281d670b125049dcae58f32b7c
flyin <flying.falcon@outlook.com>
2025-03-04 18:00:33 +0100

    [96] continue implementing Basic Notification: Create NotificationActivity and its layout. Have a  notification channel created in NotificationTestActivity, and send a notification with content title, text, small icon, large icon. Add PendingIntent to start the created NotificationActivity when user click the notification. Add setAutoCancel(set to true) to auto cancel the notification when it's clicked. Register NotificationActivity in AndroidManifest.xml.

--- app/src/main/java/com/example/multimediatest/NotificationTestActivity.kt
+++ app/src/main/java/com/example/multimediatest/NotificationTestActivity.kt
@@ -2,7 +2,9 @@ package com.example.multimediatest
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.BitmapFactory
 import android.os.Build
 import android.os.Bundle
@@ -29,11 +31,15 @@ class NotificationTestActivity : AppCompatActivity() {
         }
 
         binding.sendNotice.setOnClickListener {
+            val intent = Intent(this, NotificationActivity::class.java)
+            val pi = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
             val notification = NotificationCompat.Builder(this, "normal")
                 .setContentTitle("This is content title")
                 .setContentText("This is content text")
                 .setSmallIcon(R.drawable.small_icon)
                 .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.large_icon))
+                .setContentIntent(pi)
+                .setAutoCancel(true)
                 .build()
             manager.notify(1, notification)
         }
